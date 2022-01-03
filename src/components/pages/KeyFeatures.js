b76f27c88075184ed752d3256fb5c2b9d8d5a1b1
Nivetha <andremasilva1991@gmail.com>
2022-01-03 22:04:52 +0000

    Add index.html

---
+++ src/components/pages/KeyFeatures.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const KeyFeatures = () => {
+  return (
+    <div className="page-heading">
+      {" "}
+      <h1>Key Features</h1>
+    </div>
+  );
+};
+
+export default KeyFeatures;
